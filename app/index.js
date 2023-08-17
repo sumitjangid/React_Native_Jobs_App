7a158cdce17cb01cd9ca064ed1352ccf00d208cd
Sumit Jangid <sumitjangid@Sumits-Air.attlocal.net>
2023-08-17 13:28:58 -0700

    Add welcome screen with Rapid Api functionality

--- app/index.js
+++ app/index.js
@@ -8,6 +8,8 @@ import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components
 const Home = () => {
 
     const router = useRouter();
+    const [searchTerm, setSearchTerm] = useState("");
+
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -31,7 +33,15 @@ const Home = () => {
                         padding: SIZES.medium
                     }}
                 >
-                    <Welcome />
+                    <Welcome 
+                        searchTerm={searchTerm}
+                        setSearchTerm={setSearchTerm}
+                        handleClick={() => {
+                            if(searchTerm){
+                                router.push(`/search/${searchTerm}`)
+                            }
+                        }}
+                    />
                     <Popularjobs />
                     <Nearbyjobs />
                 </View>
--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,33 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ["babel-preset-expo"],
+//     plugins: [
+//       "@babel/plugin-proposal-export-namespace-from",
+//       "react-native-reanimated/plugin",
+//       require.resolve("expo-router/babel"),
+//     ],
+//   };
+// };
+
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "@babel/plugin-proposal-export-namespace-from",
-      "react-native-reanimated/plugin",
-      require.resolve("expo-router/babel"),
+      [
+              "@babel/plugin-proposal-export-namespace-from",
+              "react-native-reanimated/plugin",
+              require.resolve("expo-router/babel"),
+            ],
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
     ],
   };
 };
